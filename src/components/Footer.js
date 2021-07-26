248325a5550a03b7801f636e97cbcf8f756cb665
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-07-26 10:41:07 +0300

    new repo

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component{
+    render(){
+        return(
+            <footer>
+            <p>Author: Rahaf Al-Jazzazi &copy;</p>
+            </footer>
+            )
+    
+    }
+}
+
+export default Footer;
