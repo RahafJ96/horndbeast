0f8b48f11c8107978e29411ae9e0c47fe6e6553e
Rahaf Al-Jazzazi <rahafjazz@gmail.com>
2021-07-27 17:09:52 +0300

    solving lab03

--- src/components/HorndBeast.js
+++ src/components/HorndBeast.js
@@ -2,61 +2,59 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "../App.css";
 import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+import SelectedBeast from './SelectedBeast';
+
 
 
 class HornedBeasts extends React.Component {
-    
-    constructor(props){
-        super(props)
-        this.state={
-            numberOfLike: 0,
-            numberOfDisLike: 0
 
+    constructor(props) {
+        super(props)
+        this.state = {
+            numberOfLike: 0,
         }
     }
 
-    incrementNumber=()=>{
+    incrementNumber = () => {
         this.setState(
             {
-                numberOfLike: this.state.numberOfLike +1
+                numberOfLike: this.state.numberOfLike + 1
             }
         )
     }
-    // decrementNumber=()=>{
-    //     this.setState(
-    //         {
-    //             numberOfLike: this.state.numberOfLike -1
-    //         }
-    //     )
-    // }
-   
+  
+    clickFunc = () => {
+        this.setState({
+            click: this.state.click + 1
+        })
+    }
+    cancelFunc = () =>{
+        this.setState({
+            click: this.state.click - 1
+        })
+    }
+
+
+
     render() {
         return (
             <>
-                {/* <h2>{this.props.title}</h2>
-                <img src={this.props.imgUrl} />
-                <p>{this.props.description}</p> */}
-                <section>
                 <Col>
+                    <Card style={{ width: '20rem', height: "38rem" }}>
+                        <Card.Img onClick={this.incrementNumber} variant="top" src={this.props.image_url} />
+                        <Card.Body>
+                            <Card.Title>{this.props.title}</Card.Title>
+                            <Card.Text >
+                                {this.props.description}<br />
+                                Number of likes ❤️: {this.state.numberOfLike}
+                            </Card.Text>
+                            <SelectedBeast selectFun={this.clickFunc} cancelFunc={this.cancelFunc} image_url={this.props.image_url} description={this.props.description} title={this.props.title} />
 
-                <Card style={{ width: '18rem' , height: "35rem"  }}>
-                    <Card.Img onClick={this.incrementNumber} variant="top" src={this.props.image_url} />
-                    <Card.Body>
-                        <Card.Title>{this.props.title}</Card.Title>
-                        <Card.Text >
-                        {this.props.description}
-                        </Card.Text>
-                    
-                        <Button onClick={this.incrementNumber} variant="primary">
-                        Number of likes ❤️: {this.state.numberOfLike}
-                        </Button>
-                        {/* <Button onClick={this.decrementNumber} variant="primary">💔 - {this.state.numberOfDisike}</Button> */}
-                    </Card.Body>
-                </Card>
+                        </Card.Body>
+                    </Card>
                 </Col>
-                </section>
             </>
         )
     }
